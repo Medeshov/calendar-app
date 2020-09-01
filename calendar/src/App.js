0b0797fc2d721e57cd438092f8f57355ab2e16f0
Kayrat <kayrat.medeshov@gmail.com>
2020-09-02 00:47:34 +0700

    output logic

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import moment from "moment";
 
-function App() {
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+const getDates = (startDate, stopDate) => {
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(currentDate);
+    currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+};
+
+var MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+var DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const getMonthsFirstAndLastDaysForYear = (year) => {
+  var yearMonths = [];
+  for (var i = 0; i < 12; i++) {
+    yearMonths.push({
+      monthName: MONTHS[i],
+      monthDays: getDates(new Date(year, i, 1), new Date(year, i + 1, 0)),
+    });
+  }
+
+  return yearMonths;
+};
+
+const DayButton = ({ dayDate }) => {
+  return <button>{`${DAYS[dayDate.getDay()]} - ${dayDate.getDate()}`}</button>;
+};
+
+const MonthBlock = ({ monthName, children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{monthName}</h1>
+      <div>{children}</div>
     </div>
   );
+};
+
+class App extends React.Component {
+  render() {
+    var yearMonths = getMonthsFirstAndLastDaysForYear(2020);
+
+    var result = yearMonths.map((month) => {
+      var days = month.monthDays.map((day) => <DayButton dayDate={day} />);
+      return <MonthBlock monthName={month.monthName}>{days}</MonthBlock>;
+    });
+    return <div className="App">{result}</div>;
+  }
 }
 
 export default App;
--- calendar/src/index.js
+++ calendar/src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const getDates = (startDate, stopDate) => {
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(currentDate);
+    currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+};
+
+const getMonthsFirstAndLastDaysForYear = (year) => {
+  var year = {};
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  months = [];
+  for (var i = 0; i < 12; i++) {
+    // year[i.toString()] = {
+    //   firstDay: new Date(year, i, 1),
+    //   lastDay: new Date(year, i + 1, 0),
+    // };
+    months.push({
+      firstDay: new Date(year, i, 1),
+      lastDay: new Date(year, i + 1, 0),
+    });
+  }
+
+  return months;
+};
